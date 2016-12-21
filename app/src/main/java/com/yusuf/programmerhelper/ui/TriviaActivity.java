58bb6c198167374f2557713ea977627fe6c7da1d
Yusuf Qedan <yusuf9191@gmail.com>
2016-12-20 16:46:52 -0800

    add layout for trivia activity and bind views

--- app/src/main/java/com/yusuf/programmerhelper/ui/TriviaActivity.java
+++ app/src/main/java/com/yusuf/programmerhelper/ui/TriviaActivity.java
@@ -3,6 +3,9 @@ package com.yusuf.programmerhelper.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.yusuf.programmerhelper.R;
 import com.yusuf.programmerhelper.models.Topic;
@@ -12,7 +15,17 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-public class TriviaActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class TriviaActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.question) TextView mQuestionTextView;
+    @Bind(R.id.status) TextView mStatusTextView;
+    @Bind(R.id.choice1) Button mChoice1;
+    @Bind(R.id.choice2) Button mChoice2;
+    @Bind(R.id.choice3) Button mChoice3;
+    @Bind(R.id.choice4) Button mChoice4;
+
     private static final String TAG = TriviaActivity.class.getSimpleName();
     private ArrayList<TriviaQuestion> triviaQuestions;
 
@@ -21,9 +34,27 @@ public class TriviaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
+        ButterKnife.bind(this);
         Topic topic = Parcels.unwrap(getIntent().getParcelableExtra("topic"));
         setTitle(topic.getTopicTitle());
         triviaQuestions = topic.getTriviaQuestions();
         Log.d(TAG, "onCreate: " + triviaQuestions.get(0).getQuestion());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mChoice1) {
+
+        }
+        if (v == mChoice2) {
+
+        }
+        if (v == mChoice3) {
+
+        }
+        if (v == mChoice4) {
+
+        }
     }
 }
