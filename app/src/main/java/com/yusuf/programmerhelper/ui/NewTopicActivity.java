1f198773889528ed891fafcafd181a82747f4207
Yusuf Qedan <yusuf9191@gmail.com>
2017-01-28 12:20:47 -0800

    edit new topic layout

--- app/src/main/java/com/yusuf/programmerhelper/ui/NewTopicActivity.java
+++ app/src/main/java/com/yusuf/programmerhelper/ui/NewTopicActivity.java
@@ -2,14 +2,26 @@ package com.yusuf.programmerhelper.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.yusuf.programmerhelper.R;
 
-public class NewTopicActivity extends AppCompatActivity {
+public class NewTopicActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_topic);
+    }
+
+    private void newTopic(){
+        final DatabaseReference topicReference = FirebaseDatabase.getInstance().getReference("topics");
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
