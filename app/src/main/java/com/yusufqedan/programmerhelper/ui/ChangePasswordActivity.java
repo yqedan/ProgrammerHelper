cdbc8ede1e1ecfa3d411e7b2cd2775f8716d19a3
Yusuf Qedan <yusuf9191@gmail.com>
2017-05-13 22:13:51 -0700

    add implentation to change user password

--- app/src/main/java/com/yusufqedan/programmerhelper/ui/ChangePasswordActivity.java
+++ app/src/main/java/com/yusufqedan/programmerhelper/ui/ChangePasswordActivity.java
@@ -1,24 +1,36 @@
 package com.yusufqedan.programmerhelper.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.yusufqedan.programmerhelper.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
-    //private static final String TAG = ChangePasswordActivity.class.getSimpleName();
+    private static final String TAG = ChangePasswordActivity.class.getSimpleName();
+    @Bind(R.id.currentPasswordEditText)
+    EditText mCurrentPassword;
     @Bind(R.id.newPasswordEditText)
     EditText mNewPassword;
     @Bind(R.id.confirmNewPasswordEditText)
     EditText mConfirmNewPassword;
     @Bind(R.id.newPasswordSubmitButton)
     Button mNewPasswordSubmitButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +39,55 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
         ButterKnife.bind(this);
         setTitle("");
         mNewPasswordSubmitButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mNewPassword.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mNewPassword.setError("Passwords do not match");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void onClick(View v) {
-        //Log.d(TAG, "onClick: ");
+        String currentPassword = mCurrentPassword.getText().toString().trim();
+        final String newPassword = mNewPassword.getText().toString().trim();
+        String confirmNewPassword = mConfirmNewPassword.getText().toString().trim();
+        String email = mUser.getEmail();
+        if (currentPassword.equals("")) {
+            mCurrentPassword.setError("Password cannot be blank");
+            return;
+        }
+
+        if(isValidPassword(newPassword,confirmNewPassword)) {
+            mAuth.signInWithEmailAndPassword(email, currentPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(ChangePasswordActivity.this, "Current Password is incorrect.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mUser.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ChangePasswordActivity.this, "Password updated", Toast.LENGTH_SHORT).show();
+                                    mCurrentPassword.setText("");
+                                    mNewPassword.setText("");
+                                    mConfirmNewPassword.setText("");
+                                } else {
+                                    Toast.makeText(ChangePasswordActivity.this, "Something went wrong try again later", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 }
